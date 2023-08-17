3563e302283e6d80d77604a242702c894c0e18e8
anuj <anuj85500@gmail.com>
2023-08-17 13:49:04 +0530

    Added Product Model Validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
+# 1. The title, description, and image URL fields aren’t empty.
+# 2. The price is a valid number not less than $0.01.
+# 3. The title is unique among all products.
+# 4. The image URL looks reasonable.
+
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  # numericality option to verify that the price is a valid number
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  # allow_blank option to avoid getting multiple error messages when the field is blank.
+  validates ​:image_url​, ​allow_blank: ​​true​, ​format: {
+    with: %r{\.(gif|jpg|png)\z}i,
+    message: 'must be a URL for GIF, PNG, JPG image.' 
+  }
 end
