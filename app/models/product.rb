9e80412a67a99d043723e47b4450b1700b0c3850
anuj <anuj85500@gmail.com>
2023-08-18 14:54:09 +0530

    Cart Creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,10 @@
 # 4. The image URL looks reasonable.
 
 class Product < ApplicationRecord
+  #{Here we declare that a product has many line items and define a 
+  #hook method named ensure_not_referenced_by_any_line_item}
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   # numericality option to verify that the price is a valid number
@@ -13,4 +17,12 @@ class Product < ApplicationRecord
     with: /\.(gif|jpg|png)\z/i,
     message: 'must be a URL for GIF, PNG or JPG image.'
   }
+  private
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items Present')
+        throw :abort
+      end
+    end
 end
