785c1cc53091bea9e709ef4b1a19063d6d5a2c42
anuj <anuj85500@gmail.com>
2023-08-18 13:32:26 +0530

    Added Catelog and fix page layout and  added price format

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,7 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav a', minimum: 4
+    assert_select 'main ul li', 3
   end
 end
